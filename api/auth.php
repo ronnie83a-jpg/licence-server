99b62a318a00c4246bc92734950f6404f75a9cdd
ronnie83a <ronnie83a@gmail.com>
2025-12-02 17:31:50 +0530

    Add files via upload

---
+++ api/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+// CHANGE THIS TOKEN BEFORE GOING LIVE
+$API_TOKEN = "SUPERSECRET_DEVTOKEN_123456789";
+
+function verify_token() {
+    global $API_TOKEN;
+
+    $sent = $_SERVER['HTTP_X_AUTH_TOKEN'] ?? '';
+    if ($sent !== $API_TOKEN) {
+        http_response_code(401);
+        echo json_encode(["error" => "Invalid API token"]);
+        exit;
+    }
+}
